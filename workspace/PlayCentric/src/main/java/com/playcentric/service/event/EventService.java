af3bf641a06927d2af4a94dcaae1d625dfcd6c28
jun3978 <weijun.ray@gmail.com>
2024-07-10 21:00:15 +0800

    event 增加模糊查詢

--- workspace/PlayCentric/src/main/java/com/playcentric/service/event/EventService.java
+++ workspace/PlayCentric/src/main/java/com/playcentric/service/event/EventService.java
@@ -1,7 +1,6 @@
 package com.playcentric.service.event;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -38,4 +37,5 @@ public class EventService {
     public void deleteEvent(Integer eventId) {
         eventRepository.deleteById(eventId);
     }
+    
 }
