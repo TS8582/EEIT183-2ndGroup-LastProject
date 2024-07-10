45bacdd42d6516aa578c4994e7f637d1d0555cd6
OwenChen0414 <owen0414chen@gmail.com>
2024-07-10 10:06:21 +0800

    memberDto 改名

--- workspace/PlayCentric/src/main/java/com/playcentric/model/member/MemberRepository.java
+++ workspace/PlayCentric/src/main/java/com/playcentric/model/member/MemberRepository.java
@@ -3,11 +3,8 @@ package com.playcentric.model.member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-
-
-
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 
@@ -21,8 +18,8 @@ public interface MemberRepository extends JpaRepository<Member, Integer> {
 
 	Page<Member> findByStatus(Short status, Pageable pageable);
 
-	// @Query("from member where status = 1 and (account like :keyword or nickname like :keyword or memName like :keyword)")
-	// Page<Member> findByKeyword(@Param("keyword") String keyword, Pageable pageable);
+	@Query("from Member where status = 1 and (account like :keyword or nickname like :keyword or memName like :keyword)")
+	Page<Member> findByKeyword(@Param("keyword") String keyword, Pageable pageable);
 
 	Page<Member> findByStatusAndAccountContainingOrNicknameContainingOrMemNameContainingOrEmailContaining(Short status, String account, String nickname, String memName, String email, Pageable pageable);
 
