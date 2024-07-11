09f65dcfed40aeba775aa05e32d28ff35858288f
TS8582 <a0958979918@gmail.com>
2024-07-11 19:27:18 +0800

    sorry my bad

---
+++ workspace/PlayCentric/src/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ["./main/resources/**/*.{html,js}"]
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
