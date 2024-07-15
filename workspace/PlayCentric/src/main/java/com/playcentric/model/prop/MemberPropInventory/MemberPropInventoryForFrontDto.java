8b28204c3b60bcd082bd740a7ca7f1dfae1c15d9
QQisCuteDog <a0970598589@gmail.com>
2024-07-15 10:30:30 +0800

    07/15 test

---
+++ workspace/PlayCentric/src/main/java/com/playcentric/model/prop/MemberPropInventory/MemberPropInventoryForFrontDto.java
@@ -0,0 +1,18 @@
+package com.playcentric.model.prop.MemberPropInventory;
+
+import com.playcentric.model.prop.Props;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class MemberPropInventoryForFrontDto {
+    private Props prop;
+    private int memId;
+    private int propQuantity;
+}
--- workspace/PlayCentric/src/main/java/com/playcentric/model/prop/MemberPropInventory/MemberPropInventoryRepository.java
+++ workspace/PlayCentric/src/main/java/com/playcentric/model/prop/MemberPropInventory/MemberPropInventoryRepository.java
@@ -1,8 +1,11 @@
-//package com.playcentric.model.prop.MemberPropInventory;
-//import java.util.Optional;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//public interface MemberPropInventoryRepository extends JpaRepository<MemberPropInventory, MemberPropInventoryId > {
-//    Optional<MemberPropInventory> findByMemberIdAndPropId(int memberId, int propId);
-//
-//}
+package com.playcentric.model.prop.MemberPropInventory;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.playcentric.model.member.Member;
+import com.playcentric.model.prop.Props;
+public interface MemberPropInventoryRepository extends JpaRepository<MemberPropInventory, MemberPropInventoryId > {
+    List<MemberPropInventory> findByIdMemId(int memId);
+}
