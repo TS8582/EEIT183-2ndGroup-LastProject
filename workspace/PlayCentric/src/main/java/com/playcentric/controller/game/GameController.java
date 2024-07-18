4effd04dfee13f5445f56623319ea9073e3b5c60
TS8582 <a0958979918@gmail.com>
2024-07-18 17:55:46 +0800

    bs ok

--- workspace/PlayCentric/src/main/java/com/playcentric/controller/game/GameController.java
+++ workspace/PlayCentric/src/main/java/com/playcentric/controller/game/GameController.java
@@ -9,9 +9,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.query.Param;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,8 +26,6 @@ import com.playcentric.service.ImageLibService;
 import com.playcentric.service.game.GameDiscountSetService;
 import com.playcentric.service.game.GameService;
 import com.playcentric.service.game.GameTypeService;
-
-import jakarta.websocket.server.PathParam;
 
 @Controller
 public class GameController {
