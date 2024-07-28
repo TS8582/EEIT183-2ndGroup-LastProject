da30bb2d0c48ef5cb454fa89b57da89394f7f1e0
QQisCuteDog <a0970598589@gmail.com>
2024-07-28 09:23:09 +0800

    倉庫權限怪怪

--- workspace/PlayCentric/src/main/java/com/playcentric/controller/prop/front/propInventory/PropInventoryController.java
+++ workspace/PlayCentric/src/main/java/com/playcentric/controller/prop/front/propInventory/PropInventoryController.java
@@ -29,7 +29,7 @@ public class PropInventoryController {
 	
 	
 	//根據會員id讀取倉庫資料
-	@GetMapping("/personal/api/PlayCentric/prop/memberPropsbyMemId")
+	@GetMapping("prop/memberPropsbyMemId")
 	@ResponseBody
 	public List<MemberPropInventoryForFrontDto> memberPropsbyMemId(int memId) {
 		return memberPropInventoryService.memberPropsbyMemId(memId);		 
