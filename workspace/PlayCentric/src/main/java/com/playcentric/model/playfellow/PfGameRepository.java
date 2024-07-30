7d2906c91d632f6bd8966b408a9760ceba31fe37
weiching12 <weichingli1208@gmail.com>
2024-07-30 21:11:07 +0800

    QAQ29-2
    前後大致完成：前台優化頁面

--- workspace/PlayCentric/src/main/java/com/playcentric/model/playfellow/PfGameRepository.java
+++ workspace/PlayCentric/src/main/java/com/playcentric/model/playfellow/PfGameRepository.java
@@ -3,6 +3,7 @@ package com.playcentric.model.playfellow;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,7 +13,7 @@ public interface PfGameRepository extends JpaRepository<PfGame, Integer> {
     List<PfGame> findByPlayFellowMemberPlayFellowId(Integer playFellowId); // 用伴遊Id去查pfgame
     
     @Query("SELECT pg FROM PfGame pg WHERE pg.game.id = :gameId")
-    List<PfGame> findByGameId(@Param("gameId") Integer gameId);
+    List<PfGame> findByGameId(@Param("gameId") Integer gameId, Sort sort);
     
     Optional<PfGame> findByPlayFellowMemberPlayFellowIdAndGameGameId(Integer playFellowId, Integer gameId);
     
