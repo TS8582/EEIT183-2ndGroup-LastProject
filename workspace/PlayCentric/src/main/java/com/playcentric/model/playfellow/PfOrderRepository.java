907b81c8eb66b64de83f8d2223f1aec696822f01
weiching12 <weichingli1208@gmail.com>
2024-07-31 13:20:17 +0800

    QAQ29-5
    小bug修改
    購買紀錄字體修正

--- workspace/PlayCentric/src/main/java/com/playcentric/model/playfellow/PfOrderRepository.java
+++ workspace/PlayCentric/src/main/java/com/playcentric/model/playfellow/PfOrderRepository.java
@@ -3,28 +3,26 @@ package com.playcentric.model.playfellow;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.playcentric.model.member.Member;
 
+import jakarta.transaction.Transactional;
+
 public interface PfOrderRepository extends JpaRepository<PfOrder, Integer> {
 
 	List<PfOrder> findByMember(Member member);
 
-	@Query(value = "SELECT o.* FROM pfOrder o " + 
-	"JOIN pfGame g ON o.pfGameId = g.pfGameId "	+ 
-			"JOIN playFellowMember pfm ON g.playFellowId = pfm.playFellowId "	+ 
-	"JOIN members m ON pfm.memId = m.memId " + 
-			"WHERE pfm.memId = :memId", 
-			nativeQuery = true)
+	@Query(value = "SELECT o.* FROM pfOrder o " + "JOIN pfGame g ON o.pfGameId = g.pfGameId "
+			+ "JOIN playFellowMember pfm ON g.playFellowId = pfm.playFellowId "
+			+ "JOIN members m ON pfm.memId = m.memId " + "WHERE pfm.memId = :memId", nativeQuery = true)
 	List<PfOrder> findOrdersByMember(@Param("memId") Integer memId);
 
-	
-	
-	
-	
+	@Modifying
+	@Transactional
+	@Query(value = "DELETE FROM pfOrder WHERE pfGameId = :pfGameId", nativeQuery = true)
+	void deleteByPfGameId(@Param("pfGameId") Integer pfGameId);
 
 }
-
-
