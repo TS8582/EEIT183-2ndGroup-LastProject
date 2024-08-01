6ad87fa0b7a3f9a317d22289e9cecd11298abdf3
jun3978 <weijun.ray@gmail.com>
2024-08-01 16:24:44 +0800

    修改DOALERT的 Z-INDEX

--- workspace/PlayCentric/src/main/resources/static/js/utils/doAlertJQ.js
+++ workspace/PlayCentric/src/main/resources/static/js/utils/doAlertJQ.js
@@ -1,13 +1,28 @@
 function doAlert(message) {
-  $('body').append(`<div class="rounded-2 position-fixed top-50 start-50 translate-middle bg-dark text-light p-2 alert-log z-3 user-select-none fs-3">${message}</div>`)
-  $('.alert-log').hide();
-  $('.alert-log').fadeIn(500);
-  setTimeout(function () {
-    $('.alert-log').fadeOut(1500);
-    setTimeout(function () {
-      $('.alert-log').remove();
-    }, 1500)
-  }, 1500)
-}
+  // 創建一個新的 div 元素，並設置其內容和樣式
+  $('body').append(`
+    <div class="rounded-2 position-fixed top-50 start-50 translate-middle bg-dark text-light p-2 alert-log user-select-none fs-3" 
+         style="z-index: 9999; opacity: 0.9;">
+      ${message}
+    </div>
+  `);
 
+  // 獲取剛創建的 alert 元素
+  const $alertLog = $('.alert-log');
+
+  // 初始隱藏 alert
+  $alertLog.hide();
+
+  // 使用淡入效果顯示 alert
+  $alertLog.fadeIn(500);
+
+  // 設置定時器來淡出並移除 alert
+  setTimeout(function () {
+    // 淡出 alert
+    $alertLog.fadeOut(1500, function() {
+      // 淡出完成後移除 alert 元素
+      $(this).remove();
+    });
+  }, 1500);
+}
 //使用jquery，標籤為不透明
